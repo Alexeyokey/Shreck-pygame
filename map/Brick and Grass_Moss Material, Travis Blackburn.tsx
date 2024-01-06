6967323c908124973814b6ca57a1929e60b00eb0
Alexeyokey <146419947+Alexeyokey@users.noreply.github.com>
2024-01-06 11:22:50 +0300

    Delete map directory

--- map/Brick and Grass_Moss Material, Travis Blackburn.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Brick and Grass_Moss Material, Travis Blackburn" tilewidth="64" tileheight="64" tilecount="121" columns="11">
- <image source="Brick and Grass_Moss Material, Travis Blackburn.jpg" width="736" height="736"/>
-</tileset>
