6f985ec3af101c2d9daa73cc7e903450f9e44489
Alexeyokey <146419947+Alexeyokey@users.noreply.github.com>
2024-01-06 11:24:22 +0300

    map reupload

---
+++ map/1669966547_3-pibig-info-p-sprait-dereva-krasivo-3.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="1669966547_3-pibig-info-p-sprait-dereva-krasivo-3" tilewidth="64" tileheight="64" tilecount="270" columns="15">
+ <image source="1669966547_3-pibig-info-p-sprait-dereva-krasivo-3.png" width="960" height="1200"/>
+</tileset>
---
+++ map/Architextures, the material editor for architects and designers — Architextures.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Architextures, the material editor for architects and designers — Architextures" tilewidth="64" tileheight="64" tilecount="132" columns="11">
+ <image source="Architextures, the material editor for architects and designers — Architextures.jpg" width="736" height="770"/>
+</tileset>
